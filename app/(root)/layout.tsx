b98279138505fe9fc7104ef7d1d5c2366e6e5dcb
jony <jonybhattcharjee886@gmail.com>
2024-12-26 22:11:13 +0600

    Rebuild process

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,6 @@
-import Footer from "@/components/shared/Footer";
-import Navbar from "@/components/shared/Navbar";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+// import Footer from "@/components/shared/Footer";
+// import Navbar from "@/components/shared/Navbar";
 import React from "react";
 
 export default function MainLayout({
@@ -9,9 +10,11 @@ export default function MainLayout({
 }>) {
   return (
     <div className="flex flex-col min-h-svh w-full ">
-      <Navbar />
-      <main className="flex-grow h-full">{children}</main>
-      <Footer />
+      {/* <Navbar /> */}
+      <main className="flex-grow h-full">
+        <h1 className="text-center font-bold text-4xl">Coming Soon...</h1>
+      </main>
+      {/* <Footer /> */}
     </div>
   );
 }
